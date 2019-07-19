a57f23bbe5e4cb0b252bc520cd3b361006bf484a
kalpltoska <kalpltoska@gmail.com>
2019-07-19 14:49:19 +0300

    Adopt libc++ technique with _LIBCPP_VARIANT_UNION

--- Public/Expected/Details/ExpectedUnion.hpp
+++ Public/Expected/Details/ExpectedUnion.hpp
@@ -14,101 +14,61 @@ namespace stdx::details
 	constexpr valueless_t valueless;
 
 	template <typename T, typename E, bool = And<VoidOrTriviallyDestructible<T>, TriviallyDestructible<E>>()>
-	union ExpectedUnion
-	{
-		explicit constexpr ExpectedUnion(valueless_t) noexcept : __Dummy()
-		{
-		}
+	union ExpectedUnion;
 
-		template <typename ...Ts>
-		explicit constexpr ExpectedUnion(std::in_place_index_t<0>, Ts&& ...Args)
-		noexcept(NothrowConstructible<T, Ts...>::value) : Value(std::forward<Ts>(Args)...)
-		{
-		}
+#define _EXPECTED_UNION(TriviallyDestructible, Destructor)                                             \
+	template <typename T, typename E>                                                                  \
+	union ExpectedUnion<T, E, TriviallyDestructible>                                                   \
+	{                                                                                                  \
+	    explicit constexpr ExpectedUnion(valueless_t) noexcept : __Dummy()                             \
+	    {                                                                                              \
+	    }                                                                                              \
+	                                                                                                   \
+	    template <typename ...Ts>                                                                      \
+	    explicit constexpr ExpectedUnion(std::in_place_index_t<0>, Ts&& ...Args)                       \
+	    noexcept(NothrowConstructible<T, Ts...>::value) : Value(std::forward<Ts>(Args)...)             \
+	    {                                                                                              \
+	    }                                                                                              \
+	                                                                                                   \
+	    template <typename ...Ts>                                                                      \
+	    explicit constexpr ExpectedUnion(std::in_place_index_t<1>, Ts&& ...Args)                       \
+	    noexcept(NothrowConstructible<Unexpected<E>, Ts...>::value) : Unex(std::forward<Ts>(Args)...)  \
+	    {                                                                                              \
+	    }                                                                                              \
+	                                                                                                   \
+	    Destructor                                                                                     \
+	                                                                                                   \
+	    valueless_t __Dummy;                                                                           \
+	    T Value;                                                                                       \
+	    Unexpected<E> Unex;                                                                            \
+	};                                                                                                 \
+	                                                                                                   \
+	template <typename E>                                                                              \
+	union ExpectedUnion<void, E, TriviallyDestructible>                                                \
+	{                                                                                                  \
+	    explicit constexpr ExpectedUnion(valueless_t) noexcept : __Dummy()                             \
+	    {                                                                                              \
+	    }                                                                                              \
+	                                                                                                   \
+	    explicit constexpr ExpectedUnion(std::in_place_index_t<0>) noexcept : ExpectedUnion(valueless) \
+	    {                                                                                              \
+	    }                                                                                              \
+	                                                                                                   \
+	    template <typename ...Ts>                                                                      \
+	    explicit constexpr ExpectedUnion(std::in_place_index_t<1>, Ts&& ...Args)                       \
+	    noexcept(NothrowConstructible<Unexpected<E>, Ts...>::value) : Unex(std::forward<Ts>(Args)...)  \
+	    {                                                                                              \
+	    }                                                                                              \
+	                                                                                                   \
+	    Destructor                                                                                     \
+	                                                                                                   \
+	    valueless_t __Dummy;                                                                           \
+	    Unexpected<E> Unex;                                                                            \
+	};                                                                                                 \
 
-		template <typename ...Ts>
-		explicit constexpr ExpectedUnion(std::in_place_index_t<1>, Ts&& ...Args)
-		noexcept(NothrowConstructible<Unexpected<E>, Ts...>::value) : Unex(std::forward<Ts>(Args)...)
-		{
-		}
 
-		valueless_t __Dummy;
-		T Value;
-		Unexpected<E> Unex;
-	};
+	_EXPECTED_UNION(true, ~ExpectedUnion() = default;)
+	_EXPECTED_UNION(false, ~ExpectedUnion() {})
 
-	template <typename T, typename E>
-	union ExpectedUnion<T, E, false>
-	{
-		explicit constexpr ExpectedUnion(valueless_t) noexcept : __Dummy()
-		{
-		}
-
-		template <typename ...Ts>
-		explicit constexpr ExpectedUnion(std::in_place_index_t<0>, Ts&& ...Args)
-		noexcept(NothrowConstructible<T, Ts...>::value) : Value(std::forward<Ts>(Args)...)
-		{
-		}
-
-		template <typename ...Ts>
-		explicit constexpr ExpectedUnion(std::in_place_index_t<1>, Ts&& ...Args)
-		noexcept(NothrowConstructible<Unexpected<E>, Ts...>::value) : Unex(std::forward<Ts>(Args)...)
-		{
-		}
-
-		~ExpectedUnion()
-		{
-		}
-
-		valueless_t __Dummy;
-		T Value;
-		Unexpected<E> Unex;
-	};
-
-	template <typename E>
-	union ExpectedUnion<void, E, true>
-	{
-		explicit constexpr ExpectedUnion(valueless_t) noexcept : __Dummy()
-		{
-		}
-
-		explicit constexpr ExpectedUnion(std::in_place_index_t<0>) noexcept : ExpectedUnion(valueless)
-		{
-		}
-
-		template <typename ...Ts>
-		explicit constexpr ExpectedUnion(std::in_place_index_t<1>, Ts&& ...Args)
-		noexcept(NothrowConstructible<Unexpected<E>, Ts...>::value) : Unex(std::forward<Ts>(Args)...)
-		{
-		}
-
-		valueless_t __Dummy;
-		Unexpected<E> Unex;
-	};
-
-	template <typename E>
-	union ExpectedUnion<void, E, false>
-	{
-		explicit constexpr ExpectedUnion(valueless_t) noexcept : __Dummy()
-		{
-		}
-
-		explicit constexpr ExpectedUnion(std::in_place_index_t<0>) noexcept : ExpectedUnion(valueless)
-		{
-		}
-
-		template <typename ...Ts>
-		explicit constexpr ExpectedUnion(std::in_place_index_t<1>, Ts&& ...Args)
-		noexcept(NothrowConstructible<Unexpected<E>, Ts...>::value) : Unex(std::forward<Ts>(Args)...)
-		{
-		}
-
-		~ExpectedUnion()
-		{
-		}
-
-		valueless_t __Dummy;
-		Unexpected<E> Unex;
-	};
-
+#undef _EXPECTED_UNION
 }
